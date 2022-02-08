dc2d539836b12fa51a5c267e10f7be644f86d9b2
JettiJAM <jettijam@gmail.com>
2022-02-08 20:14:09 +0900

    apply code review

--- thumbnail.py
+++ thumbnail.py
@@ -6,6 +6,7 @@ import random
 import sys
 
 class ThumbnailCreator():
+    space_between_lines = 10
     def __init__(self, texts):
         fnt = ImageFont.truetype("./font.ttf", encoding="UTF-8", size=40)
         texts = [" "]+texts+[" "]
@@ -21,22 +22,15 @@ class ThumbnailCreator():
         #  while fnt.getsize(texts[0])[0] < maxW:
             #  texts[0] += " "
 
-        totH += 10*(len(texts)-2)
+        totH += self.space_between_lines*(len(texts)-1)
         self.im = Image.new("RGB", (maxW, totH))
        
-        (w,h) = self.im.size
-        pxs = self.im.load()
-        color1 = randomColor()
-        color2 = randomColor()
-        for y in range(h):
-            r = ((h-y) * color2[0] + y * color1[0]) / h
-            g = ((h-y) * color2[1] + y * color1[1]) / h
-            b = ((h-y) * color2[2] + y * color1[2]) / h
-            for x in range(w):
-                pxs[x,y] = (int(r), int(g), int(b))
-        
+        upper_color = self.randomColor()
+        lower_color = self.randomColor()
+        self.gradation(upper_color, lower_color)
         draw = ImageDraw.Draw(self.im)
-        draw.multiline_text((0,0), "\n".join(texts), font=fnt, fill=(255, 255, 255), align="left", spacing=10)
+        draw.multiline_text((0,0), "\n".join(texts), font=fnt, fill=(255, 255, 255), 
+                                align="left", spacing=self.space_between_lines)
 
     def show(self):
         self.im.show()
@@ -44,6 +38,15 @@ class ThumbnailCreator():
     def save(self, path):
         self.im.save(path, "JPEG")
 
-def randomColor():
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    def randomColor(self):
+        return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
+    def gradation(self, upper_color, lower_color):
+        (w,h) = self.im.size
+        pxs = self.im.load()
+        for y in range(h):
+            r = ((h-y) * lower_color[0] + y * upper_color[0]) / h
+            g = ((h-y) * lower_color[1] + y * upper_color[1]) / h
+            b = ((h-y) * lower_color[2] + y * upper_color[2]) / h
+            for x in range(w):
+                pxs[x,y] = (int(r), int(g), int(b))
